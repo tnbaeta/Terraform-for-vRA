45354dc604fe4e8589e23b9fa586879cb75cfc57
Tiago Baeta <tnbaeta@gmail.com>
2022-08-16 20:03:15 -0300

    No NSG

--- azure/virtual_network/main.tf
+++ azure/virtual_network/main.tf
@@ -13,22 +13,16 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "brd_rg" {
+  count    = var.create_rg == true ? 1 : 0
   name     = var.rg_name
   location = var.location
 
-	lifecycle {
-    # The AMI ID must refer to an existing AMI that has the tag "nomad-server".
-    postcondition {
-      condition     = var.create_rg == true
-      error_message = "Create RG must be checked"
-    }
-  }
 }
 
 resource "azurerm_virtual_network" "brd_vn" {
   name                = var.vn_name
-  location            = azurerm_resource_group.brd_rg.location
-  resource_group_name = azurerm_resource_group.brd_rg.name
+  location            = var.location
+  resource_group_name = var.rg_name
   address_space       = var.address_space
   dns_servers         = var.dns_servers
 
@@ -40,7 +34,7 @@ resource "azurerm_virtual_network" "brd_vn" {
 resource "azurerm_subnet" "brd_subnet" {
   name           = "${var.subnet[count.index].name}"
   virtual_network_name = azurerm_virtual_network.brd_vn.name
-  resource_group_name = azurerm_resource_group.brd_rg.name
+  resource_group_name = var.rg_name
   address_prefixes = "${var.subnet[count.index].address}"
   count          = "${length(var.subnet)}"
 }
