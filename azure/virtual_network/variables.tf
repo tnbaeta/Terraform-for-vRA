ebcdc8f547cc0ede667266f7623ff84159f7db93
Tiago Baeta <tnbaeta@gmail.com>
2022-08-16 18:19:38 -0300

    Changed blueprint version back to 1.0

--- azure/virtual_network/variables.tf
+++ azure/virtual_network/variables.tf
@@ -1,31 +1,38 @@
 variable "location" {
   type = string
-  default = "westus2"
+  description = "Região"
+  default = "eastus"
 }
 
 variable "create_rg" {
   type = bool
+  description = "Criar novo Grupo de Recursos"
   default = false
 }
 variable "rg_name" {
   type = string
+  description = "Nome do Grupo de Recursos"
   default = "brd_rg"
 }
 
 variable "vn_name" {
   type = string
+  description = "Nome da Rede Virtual"
 	default = "brd_vn"
 }
 
 variable "address_space" {
+  description = "Espaço de Endereço (CIDR)"
   default = ["10.0.0.0/16"]
 }
 
 variable "dns_servers" {
+  description = "Servidor(es) de DNS"
   default = ["10.10.10.10", "10.20.0.0"]
 }
 
 variable "environment" {
+  description = "Ambiente"
 	default = "prod"
 }
 
@@ -37,7 +44,9 @@ variable "subnet" {
   default = [
     {
       name = "brd-1"
-      address = ["addr-1"]
+      address = ["10.0.1.0/24"],
+      name = "brd-2"
+      address = ["10.0.2.0/24"]
     }
   ]
 }
