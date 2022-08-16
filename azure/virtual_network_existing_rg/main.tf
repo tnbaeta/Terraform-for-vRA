66eabc4130f831e6cca4980cb43be9eabb8c3fb0
Tiago Baeta <tnbaeta@gmail.com>
2022-08-16 12:31:17 +0000

    Changed existing vn

--- azure/virtual_network_existing_rg/main.tf
+++ azure/virtual_network_existing_rg/main.tf
@@ -2,15 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.15.1"
+      version = "3.18.0"
     }   
   }
-#  cloud {
-#    organization = "illusion-factory-labs"
-#    workspaces {
-#      name = "brd"
-#    }
-#  }
 }
 
 provider "azurerm" {
@@ -18,16 +12,10 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_network_security_group" "brd_nsg" {
-  name                = var.nsg_name
-  location            = var.location
-  resource_group_name = var.rg_name
-}
-
 resource "azurerm_virtual_network" "brd_vn" {
   name                = var.vn_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.brd_rg.name
+  resource_group_name = var.rg_name
   address_space       = var.address_space
   dns_servers         = var.dns_servers
 
@@ -37,9 +25,9 @@ resource "azurerm_virtual_network" "brd_vn" {
 }
 
 resource "azurerm_subnet" "brd_subnet" {
-  name           = "${var.subnet_names[count.index]}"
+  name           = "${var.subnet[count.index].name}"
   virtual_network_name = azurerm_virtual_network.brd_vn.name
-  resource_group_name = azurerm_resource_group.brd_rg.name
-  address_prefixes = ["${var.address_prefixes[count.index]}"]
-  count          = "${length(var.subnet_names)}"
+  resource_group_name = var.rg_name
+  address_prefixes = "${var.subnet[count.index].address}"
+  count          = "${length(var.subnet)}"
 }
--- azure/virtual_network_existing_rg/variables.tf
+++ azure/virtual_network_existing_rg/variables.tf
@@ -1,53 +1,39 @@
 variable "location" {
   type = string
-  description = "Região"
   default = "eastus"
 }
 
 variable "rg_name" {
   type = string
-  description = "Resource Group Name"
   default = "brd_rg"
-}
-variable "nsg_name" {
-  type = string
-  description = "Network Security Group Name"
-	default = "brd_nsg"
 }
 
 variable "vn_name" {
   type = string
-  description = "Virtual Network Name"
 	default = "brd_vn"
 }
 
 variable "address_space" {
-  description = "Address Space"
   default = ["10.0.0.0/16"]
 }
 
 variable "dns_servers" {
-  description = "DNS Servers"
   default = ["10.10.10.10", "10.20.0.0"]
 }
 
-variable "subnet_names" {
-  description = "Subnet Names"
-	default = ["brd_subnet-01","brd_subnet-02"]
-}
-
-variable "address_prefixes" {
-  description = "Address Prefixes"
-  default = ["10.0.1.0/24","10.0.2.0/24"]
-}
-
 variable "environment" {
-  description = "Environment"
 	default = "prod"
 }
 
-variable "subnet_count" {
-  type = number
-  description = "Number of Subnets"
-  default = 2
+variable "subnet" {
+  type = list(object({
+    name = string
+    address = list(string)
+    }))
+  default = [
+    {
+      name = "brd-1"
+      address = ["addr-1"]
+    }
+  ]
 }
